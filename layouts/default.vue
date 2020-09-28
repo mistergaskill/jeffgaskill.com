75e88b7645702fc6fd3b9e7f866de1510f2d4632
Jeff Gaskill <mistergaskill@gmail.com>
2020-09-28 14:17:12 -0700

    Sync page routing with nav tab selection

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <md-tabs>
-      <md-tab id="tab-about" md-label="About" to="/"></md-tab>
+    <md-tabs md-sync-route>
+      <md-tab id="tab-about" md-label="About" to="/" exact></md-tab>
       <md-tab id="tab-reading" md-label="Reading" to="/reading"></md-tab>
     </md-tabs>
     <Nuxt />
@@ -14,6 +14,10 @@ import VueMaterial from 'vue-material'
 Vue.use(VueMaterial)
 
 export default {
-  
+  computed: {
+      currentRouteName() {
+        return this.$route.name;
+      }
+  }
 }
 </script>
