ea090ceadbb304dbe7debbf045b775a272545cbe
Jeff Gaskill <mistergaskill@gmail.com>
2020-09-28 14:41:37 -0700

    Expose reading list via data binding

--- pages/reading.vue
+++ pages/reading.vue
@@ -1,26 +1,44 @@
 <template>
   <div>
     <h1>Recommended Reading</h1>
-    <div>
-      <a href="http://www.paulgraham.com/makersschedule.html">Maker's Schedule, Manager's Schedule</a>
-      Paul Graham
-    </div>
-    <div>
-      <a
-        href="https://www.staceybarr.com/measure-up/how-to-make-okrs-measurable/"
-      >How to Make OKRs Measurable</a>
-      Stacey Barr
-    </div>
-    <div>
-      <a
-        href="https://pragprog.com/titles/tpp20/the-pragmatic-programmer-20th-anniversary-edition/"
-      >The Pragmatic Programmer</a>
-      David Thomas, Andrew Hunt
+    <div v-for="item in readingList" :key="item.title">
+      
+      <a :href="item.actions[0].href">{{item.title}}</a>
+      {{item.author}}
     </div>
   </div>
 </template>
 <script>
 export default {
   head: { title: "Jeff Gaskill, Reading" },
+  data () {
+    return {
+      readingList: [{
+        title: 'Maker\'s Schedule, Manager\'s Schedule',
+        author: 'Paul Graham',
+        description: 'Essential reading for every manager and any engineer who has too many meetings.',
+        actions: [{
+          label: 'Read',
+          href: 'http://www.paulgraham.com/makersschedule.html'
+        }]
+      }, {
+        title: 'How to Make OKRs Measurable',
+        author: 'Stacey Barr',
+        description: '',
+        actions: [{
+          label: 'Read',
+          href: 'https://www.staceybarr.com/measure-up/how-to-make-okrs-measurable/'
+        }]
+      }, {
+        title: 'The Pragmatic Programmer',
+        author: 'David Thomas, Andrew Hunt',
+        description: '',
+        actions: [{
+          label: 'Buy',
+          href: 'https://pragprog.com/titles/tpp20/the-pragmatic-programmer-20th-anniversary-edition/'
+        }]
+      }]
+    }
+  }
 };
 </script>
